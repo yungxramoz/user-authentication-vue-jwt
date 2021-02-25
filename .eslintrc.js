5483260ac724a6b2ec18aa552cb1d468893c8643
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-25 20:13:00 +0100

    Setup Project

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: ['error', 'never'],
   },
-};
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+}
