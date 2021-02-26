7daa3e34052de00446a92e5160e4c664e7db67f5
Sandro Gerber <sandro-gerber@outlook.com>
2021-02-27 00:45:49 +0100

    Fix type

--- src/helpers/promise-error-handler.ts
+++ src/helpers/promise-error-handler.ts
@@ -1,4 +1,4 @@
-export function promiseErrorHandler(error: any, callback: Function) {
+export function promiseErrorHandler(error: any, callback: Function): Promise<any> {
   callback()
   const message =
     (error.response && error.response.data && error.response.data.message) ||
