2058972da8d680cfb4587b2b2f402e39f0beed4d
Sandro Gerber <sandro-gerber@outlook.com>
2021-03-12 02:14:43 +0100

    Fix components not loading on build

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import YrTextField from '@/components/atoms/YrTextField.vue'
+import YrBtn from '@/components/atoms/YrBtn.vue'
+
+import YrPasswordField from '@/components/molecules/YrPasswordField.vue'
+import YrIconBtn from '@/components/molecules/YrIconBtn.vue'
+
 Vue.config.productionTip = false
+
+Vue.component('yr-text-field', YrTextField)
+Vue.component('yr-btn', YrBtn)
+
+Vue.component('yr-password-field', YrPasswordField)
+Vue.component('yr-icon-btn', YrIconBtn)
 
 new Vue({
   router,
