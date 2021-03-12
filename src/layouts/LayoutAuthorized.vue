29856f47644fcede2dbf81e1b6ec84ef283fa2a1
Sandro Gerber <sandro-gerber@outlook.com>
2021-03-12 21:46:45 +0100

    Add dialog before logout

--- src/layouts/LayoutAuthorized.vue
+++ src/layouts/LayoutAuthorized.vue
@@ -17,14 +17,43 @@
         </v-img>
       </router-link>
       <v-spacer></v-spacer>
-      <v-tooltip left>
-        <template v-slot:activator="{ on, attrs }">
-          <yr-icon-btn v-bind="attrs" v-on="on" @click="logout" data-cy="to-logout-btn">
-            mdi-logout
-          </yr-icon-btn>
+
+      <v-dialog v-model="logoutDialog" max-width="500px" :persistent="logoutLoading" retain-focus>
+        <template v-slot:activator="{ on: dialogOn, attrs: dialogAttrs }">
+          <v-tooltip left>
+            <template v-slot:activator="{ on: tooltipOn, attrs: tooltipAttrs }">
+              <yr-icon-btn
+                v-bind="{ ...dialogAttrs, ...tooltipAttrs }"
+                v-on="{ ...dialogOn, ...tooltipOn }"
+                data-cy="to-logout-btn"
+              >
+                mdi-logout
+              </yr-icon-btn>
+            </template>
+            <span>Logout</span>
+          </v-tooltip>
         </template>
-        <span>Logout</span>
-      </v-tooltip>
+
+        <yr-dialog-card>
+          <template #title>
+            Logout
+          </template>
+
+          <template #content>
+            Are you sure you want to log out?
+          </template>
+
+          <template #actions>
+            <v-spacer></v-spacer>
+            <yr-btn text :disabled="logoutLoading" @click="logoutDialog = false">
+              Cancel
+            </yr-btn>
+            <yr-btn text :disabled="logoutLoading" :loading="logoutLoading" @click="logout">
+              Logout
+            </yr-btn>
+          </template>
+        </yr-dialog-card>
+      </v-dialog>
     </yr-app-bar>
 
     <v-main>
@@ -54,16 +83,17 @@ import { getModule } from 'vuex-module-decorators'
 
 import AuthModule from '@/store/modules/auth-module'
 
-import { YrAppBar, YrIconBtn } from '@/components'
+import { YrAppBar } from '@/components'
 
 @Component({
   components: {
     YrAppBar,
-    YrIconBtn,
   },
 })
 export default class LayoutDefault extends Vue {
   private auth: AuthModule
+  private logoutLoading = false
+  private logoutDialog = false
 
   constructor() {
     super()
@@ -71,8 +101,14 @@ export default class LayoutDefault extends Vue {
   }
 
   logout() {
-    this.auth.logout()
-    this.$router.push('/')
+    this.logoutLoading = true
+
+    //fake loading
+    setTimeout(() => {
+      this.auth.logout()
+      this.$router.push('/')
+      this.logoutLoading = false
+    }, 2000)
   }
 }
 </script>
