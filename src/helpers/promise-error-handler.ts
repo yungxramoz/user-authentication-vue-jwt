a777cb775401047e24162faabf95ef0df9f1a56d
Sandro Gerber <sandro-gerber@outlook.com>
2021-03-13 18:10:48 +0100

    Remove console log

--- src/helpers/promise-error-handler.ts
+++ src/helpers/promise-error-handler.ts
@@ -1,6 +1,5 @@
 export function promiseErrorHandler(error: any, callback: Function): Promise<any> {
   callback()
-  console.log(error.response)
   const message = (error.response && error.response.data) || error.response.data || error.toString()
   return Promise.reject(message)
 }
